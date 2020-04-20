402e30bfd54c722c25746f2663d31a3b054aa9f3
VITOR FINOTTI FERREIRA <VFFERRE@corp.ad.emb>
2020-04-20 14:59:35 -0300

    Remove logger on print() function

--- utils/misc.py
+++ utils/misc.py
@@ -8,31 +8,6 @@ import hashlib
 
 from IPython import embed
 
-#class Logger(object):
-#    def __init__(self):
-#        self._logger = None
-
-#    def init(self, logdir, name='log'):
-#        if self._logger is None:
-#            import logging
-#            if not os.path.exists(logdir):
-#                os.makedirs(logdir)
-#            log_file = os.path.join(logdir, name)
-#            if os.path.exists(log_file):
-#                os.remove(log_file)
-#            self._logger = logging.getLogger()
-#            self._logger.setLevel('INFO')
-#            fh = logging.FileHandler(log_file)
-#            ch = logging.StreamHandler()
-#            self._logger.addHandler(fh)
-#            self._logger.addHandler(ch)
-
-#    def info(self, str_info):
-#        self.init('/tmp', 'tmp.log')
-#        self._logger.info(str_info)
-#logger = Logger()
-
-#print = logger.info
 def ensure_dir(path, erase=False):
     if os.path.exists(path) and erase:
         print("Removing old folder {}".format(path))
