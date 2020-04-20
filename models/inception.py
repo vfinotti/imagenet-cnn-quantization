f8ab839b1bda8c6f837b541ef0d808581340eb78
VITOR FINOTTI FERREIRA <VFFERRE@corp.ad.emb>
2020-04-20 14:58:46 -0300

    Update inception and resnet models to torchvision latest version

--- models/inception.py
+++ models/inception.py
@@ -1,24 +1,50 @@
+from collections import namedtuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utee import misc
-from collections import OrderedDict
+from .utils import load_state_dict_from_url
 
 
 __all__ = ['Inception3', 'inception_v3']
 
 
 model_urls = {
+    # Inception v3 ported from TensorFlow
     'inception_v3_google': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
 }
 
+_InceptionOutputs = namedtuple('InceptionOutputs', ['logits', 'aux_logits'])
 
-def inception_v3(pretrained=False, model_root=None, **kwargs):
+
+def inception_v3(pretrained=False, progress=True, **kwargs):
+    r"""Inception v3 model architecture from
+    `"Rethinking the Inception Architecture for Computer Vision" <http://arxiv.org/abs/1512.00567>`_.
+    .. note::
+        **Important**: In contrast to the other models the inception_v3 expects tensors with a size of
+        N x 3 x 299 x 299, so ensure your images are sized accordingly.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        aux_logits (bool): If True, add an auxiliary branch that can improve training.
+            Default: *True*
+        transform_input (bool): If True, preprocesses the input according to the method with which it
+            was trained on ImageNet. Default: *False*
+    """
     if pretrained:
         if 'transform_input' not in kwargs:
             kwargs['transform_input'] = True
+        if 'aux_logits' in kwargs:
+            original_aux_logits = kwargs['aux_logits']
+            kwargs['aux_logits'] = True
+        else:
+            original_aux_logits = True
         model = Inception3(**kwargs)
-        misc.load_state_dict(model, model_urls['inception_v3_google'], model_root)
+        state_dict = load_state_dict_from_url(model_urls['inception_v3_google'],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+        if not original_aux_logits:
+            model.aux_logits = False
+            del model.AuxLogits
         return model
 
     return Inception3(**kwargs)
@@ -48,79 +74,78 @@ class Inception3(nn.Module):
         self.Mixed_7a = InceptionD(768)
         self.Mixed_7b = InceptionE(1280)
         self.Mixed_7c = InceptionE(2048)
-        self.group1 = nn.Sequential(
-            OrderedDict([
-                ('fc', nn.Linear(2048, num_classes))
-            ])
-        )
+        self.fc = nn.Linear(2048, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
                 import scipy.stats as stats
                 stddev = m.stddev if hasattr(m, 'stddev') else 0.1
                 X = stats.truncnorm(-2, 2, scale=stddev)
-                values = torch.Tensor(X.rvs(m.weight.data.numel()))
-                m.weight.data.copy_(values.reshape(m.weight.shape))
+                values = torch.as_tensor(X.rvs(m.weight.numel()), dtype=m.weight.dtype)
+                values = values.view(m.weight.size())
+                with torch.no_grad():
+                    m.weight.copy_(values)
             elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         if self.transform_input:
-            x = x.clone()
-            x[0] = x[0] * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
-            x[1] = x[1] * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
-            x[2] = x[2] * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
-        # 299 x 299 x 3
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
+        # N x 3 x 299 x 299
         x = self.Conv2d_1a_3x3(x)
-        # 149 x 149 x 32
+        # N x 32 x 149 x 149
         x = self.Conv2d_2a_3x3(x)
-        # 147 x 147 x 32
+        # N x 32 x 147 x 147
         x = self.Conv2d_2b_3x3(x)
-        # 147 x 147 x 64
+        # N x 64 x 147 x 147
         x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # 73 x 73 x 64
+        # N x 64 x 73 x 73
         x = self.Conv2d_3b_1x1(x)
-        # 73 x 73 x 80
+        # N x 80 x 73 x 73
         x = self.Conv2d_4a_3x3(x)
-        # 71 x 71 x 192
+        # N x 192 x 71 x 71
         x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # 35 x 35 x 192
+        # N x 192 x 35 x 35
         x = self.Mixed_5b(x)
-        # 35 x 35 x 256
+        # N x 256 x 35 x 35
         x = self.Mixed_5c(x)
-        # 35 x 35 x 288
+        # N x 288 x 35 x 35
         x = self.Mixed_5d(x)
-        # 35 x 35 x 288
+        # N x 288 x 35 x 35
         x = self.Mixed_6a(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = self.Mixed_6b(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = self.Mixed_6c(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = self.Mixed_6d(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = self.Mixed_6e(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         if self.training and self.aux_logits:
             aux = self.AuxLogits(x)
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = self.Mixed_7a(x)
-        # 8 x 8 x 1280
+        # N x 1280 x 8 x 8
         x = self.Mixed_7b(x)
-        # 8 x 8 x 2048
+        # N x 2048 x 8 x 8
         x = self.Mixed_7c(x)
-        # 8 x 8 x 2048
-        x = F.avg_pool2d(x, kernel_size=8)
-        # 1 x 1 x 2048
+        # N x 2048 x 8 x 8
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # N x 2048 x 1 x 1
         x = F.dropout(x, training=self.training)
-        # 1 x 1 x 2048
-        x = x.view(x.size(0), -1)
-        # 2048
-        x = self.group1(x)
-        # 1000 (num_classes)
+        # N x 2048 x 1 x 1
+        x = torch.flatten(x, 1)
+        # N x 2048
+        x = self.fc(x)
+        # N x 1000 (num_classes)
         if self.training and self.aux_logits:
-            return x, aux
+            return _InceptionOutputs(x, aux)
         return x
 
 
@@ -293,28 +318,24 @@ class InceptionAux(nn.Module):
         self.conv0 = BasicConv2d(in_channels, 128, kernel_size=1)
         self.conv1 = BasicConv2d(128, 768, kernel_size=5)
         self.conv1.stddev = 0.01
-
-        fc = nn.Linear(768, num_classes)
-        fc.stddev = 0.001
-
-        self.group1 = nn.Sequential(
-            OrderedDict([
-                ('fc', fc)
-            ])
-        )
+        self.fc = nn.Linear(768, num_classes)
+        self.fc.stddev = 0.001
 
     def forward(self, x):
-        # 17 x 17 x 768
+        # N x 768 x 17 x 17
         x = F.avg_pool2d(x, kernel_size=5, stride=3)
-        # 5 x 5 x 768
+        # N x 768 x 5 x 5
         x = self.conv0(x)
-        # 5 x 5 x 128
+        # N x 128 x 5 x 5
         x = self.conv1(x)
-        # 1 x 1 x 768
-        x = x.view(x.size(0), -1)
-        # 768
-        x = self.group1(x)
-        # 1000
+        # N x 768 x 1 x 1
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # N x 768 x 1 x 1
+        x = torch.flatten(x, 1)
+        # N x 768
+        x = self.fc(x)
+        # N x 1000
         return x
 
 
@@ -322,13 +343,10 @@ class BasicConv2d(nn.Module):
 
     def __init__(self, in_channels, out_channels, **kwargs):
         super(BasicConv2d, self).__init__()
-        self.group1 = nn.Sequential(
-            OrderedDict([
-                ('conv', nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)),
-                ('bn', nn.BatchNorm2d(out_channels, eps=0.001))
-            ])
-        )
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
 
     def forward(self, x):
-        x = self.group1(x)
+        x = self.conv(x)
+        x = self.bn(x)
         return F.relu(x, inplace=True)
