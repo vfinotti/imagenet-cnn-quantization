ee4ea61ac0cb8e95a3352e41bddddc36ae3da236
VITOR FINOTTI FERREIRA <VFFERRE@corp.ad.emb>
2020-04-20 15:11:59 -0300

    Leave only supported models on selector.py

--- utils/selector.py
+++ utils/selector.py
@@ -1,59 +1,16 @@
 from utils import misc
 import os
 from utils import dataset_loader
-#print = misc.logger.info
 from IPython import embed
 
 known_models = [
-    'mnist', 'svhn', # 28x28
-    'cifar10', 'cifar100', # 32x32
-    'stl10', # 96x96
     'alexnet', # 224x224
     'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', # 224x224
     'resnet18', 'resnet34', 'resnet50', 'resnet101','resnet152', # 224x224
-    'squeezenet_v0', 'squeezenet_v1', #224x224
+    'squeezenet_v0', 'squeezenet_v1', # 224x224
     'inception_v3', # 299x299
+    'mobilenet_v2', # 224x224
 ]
-
-def mnist(cuda=True, model_root=None):
-    print("Building and initializing mnist parameters")
-    from mnist import model, dataset
-    m = model.mnist(pretrained=os.path.join(model_root, 'mnist.pth'))
-    if cuda:
-        m = m.cuda()
-    return m, dataset_loader.get, False
-
-def svhn(cuda=True, model_root=None):
-    print("Building and initializing svhn parameters")
-    from svhn import model, dataset
-    m = model.svhn(32, pretrained=os.path.join(model_root, 'svhn.pth'))
-    if cuda:
-        m = m.cuda()
-    return m, dataset_loader.get, False
-
-def cifar10(cuda=True, model_root=None):
-    print("Building and initializing cifar10 parameters")
-    from cifar import model, dataset
-    m = model.cifar10(128, pretrained=os.path.join(model_root, 'cifar10.pth'))
-    if cuda:
-        m = m.cuda()
-    return m, dataset_loader.get10, False
-
-def cifar100(cuda=True, model_root=None):
-    print("Building and initializing cifar100 parameters")
-    from cifar import model, dataset
-    m = model.cifar100(128, pretrained=os.path.join(model_root, 'cifar100.pth'))
-    if cuda:
-        m = m.cuda()
-    return m, dataset_loader.get100, False
-
-def stl10(cuda=True, model_root=None):
-    print("Building and initializing stl10 parameters")
-    from stl10 import model, dataset
-    m = model.stl10(32, pretrained=os.path.join(model_root, 'stl10.pth'))
-    if cuda:
-        m = m.cuda()
-    return m, dataset_loader.get, False
 
 def alexnet(cuda=True, model_root=None):
     print("Building and initializing alexnet parameters")
@@ -159,8 +116,16 @@ def squeezenet_v1(cuda=True, model_root=None):
         m = m.cuda()
     return m, dataset_loader.get, True
 
+def mobilenet_v2(cuda=True, model_root=None):
+    print("Building and initializing mobilenet_v2 parameters")
+    from models import mobilenet
+    m = mobilenet.mobilenet_v2(True, model_root)
+    if cuda:
+        m = m.cuda()
+    return m, dataset_loader.get, True
+
 def select(model_name, **kwargs):
-    assert model_name in known_models, model_name
+    assert model_name in known_models, f'Supported models are {known_models}'
     kwargs.setdefault('model_root', os.path.expanduser('~/.torch/models'))
     return eval('{}'.format(model_name))(**kwargs)
 
